9e87c63bac2e1d2e80cb8ab3843940461321bdf1
Uladzimir Horbach <uhorbach@scnsoft.com>
2025-05-26 00:36:24 +0300

    favicon and theme toggle

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,24 @@
+var isDarkTheme;
+
 const prefersDarkColorScheme = () => window && window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+	isDarkTheme = prefersDarkColorScheme();
+	initThemeToggle()
 	setParticles();
 });
 
-const setParticles = () => $('#particles').particlesBg({ invertColor: !prefersDarkColorScheme() });
+const initThemeToggle = () => {
+	var $toggle = $("#theme-toggle"); 
+	isDarkTheme = prefersDarkColorScheme();
+	$toggle.prop('checked', isDarkTheme);
+	$toggle.change(() => {
+		isDarkTheme = $toggle.is(':checked');
+		$("html").css("color-scheme", isDarkTheme ? "dark" : "light")
+		setParticles();
+	});
+};
 
+const setParticles = () => $('#particles').particlesBg({ invertColor: !isDarkTheme });
+
+initThemeToggle();
 setParticles();
